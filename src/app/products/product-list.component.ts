8ad60c72ecb3a373878794445bfcb404881581b2
Franck Mukendi <muke.apps@gmail.com>
2018-01-17 18:33:10 -0500

    working on it

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,103 +6,116 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { ModalProductComponent } from './modal-product/modal-product.component';
-import {MatDialog} from '@angular/material';
+import { MatDialog } from '@angular/material';
 import { ModalMaterialDesignComponent } from './modal-material-design/modal-material-design.component';
 
 
 @Component({
-    moduleId: module.id.toString(),
-    templateUrl: 'product-list.component.html'
+  moduleId: module.id.toString(),
+  templateUrl: 'product-list.component.html'
 })
 
 export class ProductListComponent implements OnInit, OnDestroy {
-    products: any[];
-    subscription: Subscription;
-    bsModalRef: BsModalRef;
+  products: any[];
+  subscription: Subscription;
+  bsModalRef: BsModalRef;
 
-    minDate = new Date(2017, 5, 10);
-    maxDate = new Date(2018, 9, 15);
-    bsValue: Date = new Date();
-    bsRangeValue: any = [new Date(2017, 7, 4), new Date(2017, 7, 20)];
+  minDate = new Date(2017, 5, 10);
+  maxDate = new Date(2018, 9, 15);
+  bsValue: Date = new Date();
+  bsRangeValue: any = [new Date(2017, 7, 4), new Date(2017, 7, 20)];
 
-    @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
+  @ViewChild(CalendarComponent) myCalendar: CalendarComponent;
 
-    animal: string;
-    name: string;
-    calendarOptions: Object = {
-        height: 'parent',
-        fixedWeekCount : false,
-        defaultDate: new Date(),
-        editable: true,
-        eventLimit: true, // allow "more" link when too many events
-        defaultView: 'agendaWeek',
-        contentHeight : 'auto',
-        events: [
-          {
-            title: 'All Day Event',
-            start: '2016-09-01'
-          },
-          {
-            title: 'Long Event',
-            start: '2016-09-07',
-            end: '2016-09-10'
-          }
-        ]
-      };
+  animal: string;
+  name: string;
+  calendarOptions: Object = {
+    height: 'parent',
+    fixedWeekCount: false,
+    defaultDate: new Date(),
+    editable: true,
+    eventLimit: true, // allow 'more' link when too many events
+    defaultView: 'agendaWeek',
+    contentHeight: 'auto',
+    events: [
+      {
+        'title': 'All Day Event',
+        'start': '2016-01-01'
+      },
+      {
+        'title': 'Long Event',
+        'start': '2016-01-07',
+        'end': '2016-01-10'
+      },
+      {
+        'title': 'Repeating Event',
+        'start': '2016-01-09T16:00:00'
+      },
+      {
+        'title': 'Repeating Event',
+        'start': '2016-01-16T16:00:00'
+      },
+      {
+        'title': 'Conference',
+        'start': '2016-01-11',
+        'end': '2016-01-13'
+      }
+    ]
+  };
 
-    constructor(private modalService: BsModalService, public dialog: MatDialog) { }
-    ngOnInit() {}
-    ngOnDestroy() {}
+  constructor(private modalService: BsModalService, public dialog: MatDialog) { }
+  ngOnInit() { }
+  ngOnDestroy() { }
 
-    onCalendarInit = (value: any): void => {
+  onCalendarInit = (value: any): void => {
 
-    }
+  }
 
-    changeCalendarView(view) {
-      this.myCalendar.fullCalendar('changeView', view);
-    }
+  changeCalendarView(view) {
+    this.myCalendar.fullCalendar('changeView', view);
+  }
 
-    public goto (nextOrPrev) {
-       this.myCalendar.fullCalendar(nextOrPrev);
-    }
+  public goto(nextOrPrev) {
+    this.myCalendar.fullCalendar(nextOrPrev);
+  }
 
-    goPrev = (value: any): void => {
-      this.myCalendar.fullCalendar('prev');
-    }
+  goPrev = (value: any): void => {
+    this.myCalendar.fullCalendar('prev');
+  }
 
-    goNext = (value: any): void => {
-      this.myCalendar.fullCalendar('next');
-    }
+  goNext = (value: any): void => {
+    this.myCalendar.fullCalendar('next');
+  }
 
-    setNewDate = (): void => {
-      console.log(this.bsValue);
+  setNewDate = (): void => {
+    console.log(this.bsValue);
 
-      this.myCalendar.fullCalendar( 'gotoDate', this.bsValue );
-    }
+    this.myCalendar.fullCalendar('gotoDate', this.bsValue);
+  }
 
-    openModalWithComponent() {
-      const list = [
-        'Open a modal with component',
-        'Pass your data',
-        'Do something else',
-        '...'
-      ];
-      this.bsModalRef = this.modalService.show(ModalProductComponent);
-      this.bsModalRef.content.title = 'Modal with component';
-      this.bsModalRef.content.list = list;
-      setTimeout(() => {
-        list.push('PROFIT!!!');
-      }, 2000);
-    }
+  openModalWithComponent() {
+    const list = [
+      'Open a modal with component',
+      'Pass your data',
+      'Do something else',
+      '...'
+    ];
+    this.bsModalRef = this.modalService.show(ModalProductComponent);
+    this.bsModalRef.content.title = 'Modal with component';
+    this.bsModalRef.content.list = list;
+    setTimeout(() => {
+      list.push('PROFIT!!!');
+    }, 2000);
+  }
 
-    openModalMaterialDesginComponent(): void {
-      let dialogRef = this.dialog.open(ModalMaterialDesignComponent, {
-        width: '100%',
-        data: { name: this.name, animal: this.animal }
-      });
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-        this.animal = result;
-      });
-    }
+  openModalMaterialDesginComponent(): void {
+    let dialogRef = this.dialog.open(ModalMaterialDesignComponent, {
+      width: '100%',
+      data: { name: this.name, animal: this.animal }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
 }
